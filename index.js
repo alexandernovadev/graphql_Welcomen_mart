828987219998d13e0aee410c65305e64f9051790
alexsk88 <titoantifa69@gmail.com>
2022-03-10 18:36:43 -0500

    [add] add prettier and index filex

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+
+// Begin App
+const app = express()
+const port = process.env.port || 3000
 
 // definiendo el esquema
 const schema = buildSchema(`
@@ -20,7 +26,7 @@ const resolvers = {
   },
 }
 
-// Ejecutar el query hello
+/* Ejecutar el query hello
 graphql({
   schema: schema,
   source: '{ saludo }',
@@ -31,4 +37,17 @@ graphql({
 })
 .catch((e) => {
   console.log(e)
+})*/
+
+app.use(
+  '/api',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+)
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
